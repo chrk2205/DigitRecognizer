28aa4a24bd79a97b834f120444237b09c82b40b9
Rohith Kumar Chandragiri <117575312+mfw-rk@users.noreply.github.com>
2025-02-22 15:00:24 +0900

    nits and clear

--- evaluate.py
+++ evaluate.py
@@ -1,64 +1,21 @@
 import torch
-from model import NeuralNetwork
-from train import get_model
-from data.dataloader import MNISTTestDataLoader
+from models.cnn.model import Model
+from data.dataloader import TestDataLoader
 from torch.utils.data import DataLoader
 import pandas as pd
-import matplotlib.pyplot as plt
-
-
-def evaluate():
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load("model.pth"))
-    model.eval()
-
-    x_test = load_test_data()
-    y_test = model(x_test[0])
-
-    df = pd.read_csv("data/test.csv")
-
-    img = df.iloc[0, :].values.reshape(28, 28)
-
-    plt.imshow(img)
-    plt.axis("off")
-    plt.savefig("test.png")
-
-    print(y_test)
-
-# from PIL import Image
 from tqdm import tqdm
 
+
 def generate_submission():
-    model = get_model()
+    model = Model()
     model.load_state_dict(torch.load("model.pth", weights_only=True))
     model.eval()
-
-    x_test = DataLoader(MNISTTestDataLoader())
-
+    x_test = DataLoader(TestDataLoader())
     df = pd.DataFrame(columns=["ImageId", "Label"])
-
-
     for idx, x in tqdm(enumerate(x_test)):
         y_test = model(x)
-        df = df._append({"ImageId": idx+1, "Label": int(torch.argmax(y_test))}, ignore_index=True)
-        # break
-        # print(torch.argmax(y_test))
-        # # print(x.shape)
-        # x = x.squeeze()
-        # image = Image.fromarray((x.numpy() * 255).astype('uint8'))
-        # image.save(f"{idx}.png")
-
-        # plt.imshow(img)
-        # plt.axis("off")
-        # plt.savefig(f"{df.iloc[0,0]}.png")
-
-        # break
-
-
-
+        df = df._append({"ImageId": idx + 1, "Label": int(torch.argmax(y_test))}, ignore_index=True)
     df.to_csv("submission.csv", index=False)
-
-    # print(y_test)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,15 +1,20 @@
 import torch
 
 from data.dataloader import TrainDataLoader
-from models.trial.model import MNISTConvClassifier
+
+from models.cnn.model import Model
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
+
+mps_device = torch.device("mps")
 
 
 def train_model_one_epoch_batch(model, optimizer, criterion, dataloader):
     model.train()
     train_loss = 0
     for idx, (x, y) in enumerate(dataloader):
+        x = x.to(mps_device)
+        y = y.to(mps_device)
         optimizer.zero_grad()
         output = model(x)
         loss = criterion(output, y)
@@ -28,6 +33,8 @@ def evaluate_model(model, criterion, dataloader):
     hits = 0
     misses = 0
     for idx, (x, y) in enumerate(dataloader):
+        x = x.to(mps_device)
+        y = y.to(mps_device)
         output = model(x)
         loss = criterion(output, y)
         val_loss += loss.item()
@@ -41,18 +48,15 @@ def evaluate_model(model, criterion, dataloader):
     return val_loss, accuracy
 
 
-
-
 def train():
-
     dataloader = TrainDataLoader()
     train_dataloader, val_dataloader = random_split(dataloader, [0.8, 0.2])
 
     train_dataloader = DataLoader(train_dataloader)
     val_dataloader = DataLoader(val_dataloader)
 
-    model = MNISTConvClassifier()
-
+    model = Model()
+    model.to(mps_device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
 
