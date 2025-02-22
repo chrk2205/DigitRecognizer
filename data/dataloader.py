9f05ad444e06a11ce148b86c9db795d2455c73d6
Rohith Kumar Chandragiri <117575312+mfw-rk@users.noreply.github.com>
2025-02-22 16:08:13 +0900

    add: UV for package management

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,7 +1,7 @@
-
 from torch.utils.data import Dataset
 import pandas as pd
 import torch
+
 
 class TrainDataLoader(Dataset):
     def __init__(self, batch_size=10, file_path="data/train.csv"):
@@ -11,12 +11,12 @@ class TrainDataLoader(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def _one_hot(self,digit: int):
+    def _one_hot(self, digit: int):
         return [1 if i == digit else 0 for i in range(10)]
 
     def __getitem__(self, idx):
         x = torch.tensor(self.data.iloc[idx, 1:].values, dtype=torch.float)
-        x = x.view(28, 28).unsqueeze(0) # 784 -> 1, 28, 28
+        x = x.view(28, 28).unsqueeze(0)  # 784 -> 1, 28, 28
         y = torch.tensor(self._one_hot(self.data.iloc[idx, 0]), dtype=torch.float)
         return x, y
 
@@ -30,5 +30,5 @@ class TestDataLoader(Dataset):
 
     def __getitem__(self, idx):
         x = torch.tensor(self.data.iloc[idx, :].values, dtype=torch.float)
-        x = x.view(28, 28).unsqueeze(0) # 784 -> 1, 28, 28
+        x = x.view(28, 28).unsqueeze(0)  # 784 -> 1, 28, 28
         return x
