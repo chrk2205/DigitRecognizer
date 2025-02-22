23e04181df9c1d3bd8c5c0d491ed309cac5b7124
Rohith Kumar Chandragiri <117575312+mfw-rk@users.noreply.github.com>
2025-02-22 17:56:26 +0900

    feat: replace max pooling with adaptive pooling

--- models/cnn/model.py
+++ models/cnn/model.py
@@ -5,9 +5,12 @@ class Model(nn.Module):
     def __init__(self, num_classes: int = 10):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=0), # batch_size x 1 x 28 x 28 -> batch_size x 32 x 26 x 26
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=1),
+            # nn.MaxPool2d(kernel_size=(2, 2), stride=1),
+            nn.AdaptiveAvgPool2d(
+                output_size=(13, 13),
+            ), # batch_size x 32 x 26 x 26 -> batch_size x 32 x 12 x 12
             nn.Flatten(),
             nn.LazyLinear(100),
             nn.ReLU(),
