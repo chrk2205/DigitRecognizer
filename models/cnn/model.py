b5db90dbe6578bfe9b13e27c7ee39680b31ae7d3
Rohith Kumar Chandragiri <117575312+mfw-rk@users.noreply.github.com>
2025-02-22 16:09:26 +0900

    nit: model reformated

--- models/cnn/model.py
+++ models/cnn/model.py
@@ -1,3 +1,18 @@
-
 import torch.nn as nn
 
+
+class Model(nn.Module):
+    def __init__(self, num_classes: int = 10):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=1),
+            nn.Flatten(),
+            nn.LazyLinear(100),
+            nn.ReLU(),
+            nn.LazyLinear(num_classes),
+        )
+
+    def forward(self, x):
+        return self.model(x)
--- models/trial/model.py
+++ models/trial/model.py
@@ -1,18 +1,24 @@
 import torch.nn as nn
 
-class MNISTConvClassifier(nn.Module):
-    def __init__(self, input_channels: int = 1, hidden_channels: int = 32, num_classes: int = 10, hidden_dim: int = 25):
+
+class Model(nn.Module):
+    def __init__(
+        self,
+        input_channels: int = 1,
+        hidden_channels: int = 32,
+        num_classes: int = 10,
+        hidden_dim: int = 25,
+    ):
         super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(input_channels, hidden_channels, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(hidden_channels, hidden_channels*2, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(hidden_channels, hidden_channels * 2, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(2, 2), stride=2),
             nn.Flatten(),
             nn.LazyLinear(num_classes),
         )
+
     def forward(self, x):
-        y = self.model(x)
-        # print(y)
-        return y
+        return self.model(x)
